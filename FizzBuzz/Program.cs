b64b5e4547afdb446279ce4072e404fb4122f853
Tommy Waalkes <Tommy@grandcircus.co>
2018-10-24 14:58:42 -0400

    Added FizzBuzz and Others

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello Please input a number");
+                Console.WriteLine();
+                string input = Console.ReadLine();
+                int num = 0;
+                if (int.TryParse(input, out num) && ValidateNum(num))
+                {
+                    int[] nums = { 3, 5, 7 };
+
+                    Console.WriteLine(IsDivisibleByMultiple(nums, num));
+
+                    if (IsDivisibleBy(5, num) && IsDivisibleBy(7, num) && IsDivisibleBy(3, num))
+                    {
+                        Console.WriteLine("Fizz Buzz Zip");
+                    }
+                    else if(IsDivisibleBy(5, num) && IsDivisibleBy(7, num))
+                    {
+                        Console.WriteLine("Fizz Buzz");
+                    }
+                    else if (IsDivisibleBy(5, num))
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (IsDivisibleBy(7, num))
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else if(IsDivisibleBy(3, num))
+                    {
+                        Console.WriteLine("Zip");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Divisible by our if statements");
+                    }
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        public static bool IsDivisibleBy(int mod, int num)
+        {
+            bool output = num % mod == 0;
+            return output;
+        }
+
+        public static bool IsDivisibleByMultiple(int[] mod, int num)
+        {
+            foreach(int n in mod)
+            {
+                bool isDivisible = IsDivisibleBy(n, num);
+                if(isDivisible == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool ValidateNum(int n)
+        {
+            if(n >0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ IsAVowel/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please input a word and I will tell what is or isn't a vowel");
+            string input = Console.ReadLine();
+
+            char[] letters = input.ToCharArray();
+            foreach(char l in letters)
+            {
+                bool b = IsVowel(l);
+                if(b == true)
+                {
+                    Console.WriteLine(l + " is a vowel!");
+                }
+                else
+                {
+                    Console.WriteLine(l + " is not a vowel!" );
+                }
+            }
+        }
+
+        public static bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            if (vowels.Contains(c))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
